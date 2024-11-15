d794108c38e8113033ec638cf9451035eee7b306
yousefshref <yb2005at@gmail.com>
2024-11-15 22:57:01 +0200

    dsd

--- src/Variables/server.jsx
+++ src/Variables/server.jsx
@@ -1,3 +1,3 @@
 // export const api = 'http://127.0.0.1:8000/'
-export const api = 'http://192.168.1.99:8000/'
-// export const api = 'https://abdelrahmanecommerce.pythonanywhere.com/'
+// export const api = 'http://192.168.1.99:8000/'
+export const api = 'https://abdelrahmanecommerce.pythonanywhere.com/'
