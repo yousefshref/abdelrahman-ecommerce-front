aa090babb9163c103cac0ae1b280a7e26c80f323
yousefshref <yb2005at@gmail.com>
2024-11-18 14:03:23 +0200

    test

--- src/Components/Products/Products.jsx
+++ src/Components/Products/Products.jsx
@@ -6,6 +6,7 @@ import { ProductsContextProvider } from "../../Contexts/ProductsContext";
 import { productDetails } from "../../Variables/pathes";
 import ProductCard from "./ProductCard";
 import { Box, Flex, Skeleton } from "@chakra-ui/react";
+import ProductCardSkeleton from "../ProductCardSkeleton";
 
 const Products = () => {
   const productsContext = useContext(ProductsContextProvider);
@@ -34,46 +35,47 @@ const Products = () => {
       <div className="mt-5 grid gap-3 lg:grid-cols-4 md:grid-cols-3 grid-cols-2">
         {loading ? (
           Array.from({ length: 4 }).map((_, index) => (
-            <Box className="flex flex-col gap-2">
-              <Skeleton
-                height="300px"
-                width="300px"
-                borderRadius="10px"
-                startColor="gray.300"
-                endColor="gray.400"
-              />
-              <Skeleton
-                height="15px"
-                width="150px"
-                borderRadius="10px"
-                startColor="gray.300"
-                endColor="gray.400"
-              />
-              <Flex gap={5}>
-                <Skeleton
-                  height="30px"
-                  width="140px"
-                  borderRadius="10px"
-                  startColor="gray.300"
-                  endColor="gray.400"
-                />
-                <Skeleton
-                  height="30px"
-                  width="140px"
-                  borderRadius="10px"
-                  startColor="gray.300"
-                  endColor="gray.400"
-                />
-              </Flex>
-              <Skeleton
-                height="10px"
-                width="120px"
-                borderRadius="10px"
-                startColor="gray.300"
-                endColor="gray.400"
-                className="mt-3"
-              />
-            </Box>
+            // <Box className="flex flex-col gap-2">
+            //   <Skeleton
+            //     height="300px"
+            //     width="300px"
+            //     borderRadius="10px"
+            //     startColor="gray.300"
+            //     endColor="gray.400"
+            //   />
+            //   <Skeleton
+            //     height="15px"
+            //     width="150px"
+            //     borderRadius="10px"
+            //     startColor="gray.300"
+            //     endColor="gray.400"
+            //   />
+            //   <Flex gap={5}>
+            //     <Skeleton
+            //       height="30px"
+            //       width="140px"
+            //       borderRadius="10px"
+            //       startColor="gray.300"
+            //       endColor="gray.400"
+            //     />
+            //     <Skeleton
+            //       height="30px"
+            //       width="140px"
+            //       borderRadius="10px"
+            //       startColor="gray.300"
+            //       endColor="gray.400"
+            //     />
+            //   </Flex>
+            //   <Skeleton
+            //     height="10px"
+            //     width="120px"
+            //     borderRadius="10px"
+            //     startColor="gray.300"
+            //     endColor="gray.400"
+            //     className="mt-3"
+            //   />
+            // </Box>
+            <ProductCardSkeleton key={index} />
           ))
         ) : products?.map((product) => (
           <div key={product?.id}>
