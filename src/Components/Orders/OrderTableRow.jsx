7ebc29134a33b265e2334c1ad63b13bf0507348d
yousefshref <yb2005at@gmail.com>
2024-12-16 12:03:24 +0200

    fdd

--- src/Components/Orders/OrderTableRow.jsx
+++ src/Components/Orders/OrderTableRow.jsx
@@ -117,7 +117,7 @@ const OrderTableRow = ({ order, index }) => {
                 {/* <td className="border p-2 text-start">{order.state_details?.name}</td> */}
                 {/* <td className="border p-2 text-wrap text-start w-[200px]">{order.address}</td> */}
                 {/* <td className="border p-2 text-wrap text-start w-[200px]">{order.payment_method}</td> */}
-                <td className="border p-2 text-wrap text-start text-xs">{
+                <td className="border p-2 text-wrap text-start">{
                     order.is_fast_shipping
                         ?
                         <div className='text-green-600 flex gap-2 items-center'>
--- src/pages/Admin/Orders/AdminOrders.jsx
+++ src/pages/Admin/Orders/AdminOrders.jsx
@@ -240,7 +240,7 @@ const AdminOrders = () => {
                                 {/* <th className="border p-2 text-nowrap text-start w-[100px]">المحافظة</th> */}
                                 {/* <th className="border p-2 text-nowrap text-start w-[200px]">العنوان</th> */}
                                 {/* <th className="border p-2 text-nowrap text-start w-[150px]">طريقة الدفع</th> */}
-                                <th className="border p-2 text-nowrap text-start text-xs w-[70px]">نوع الشحن</th>
+                                <th className="border p-2 text-nowrap text-start w-[70px]">نوع الشحن</th>
                                 <th className="border p-2 text-nowrap text-start w-[130px]">الحالة</th>
                                 {/* <th className="border p-2 text-nowrap text-start w-[200px]">كود التتبع</th> */}
                                 {/* <th className="border p-2 text-nowrap text-start w-[150px]">الاجمالي</th> */}
