673fc12e65652caf55663d74e04c94bad2714866
yousefshref <yb2005at@gmail.com>
2024-12-28 14:19:09 +0200

    sa

--- src/Variables/server.jsx
+++ src/Variables/server.jsx
@@ -1,6 +1,6 @@
-export const api = 'http://127.0.0.1:8000/'
+// export const api = 'http://127.0.0.1:8000/'
 // export const api = 'http://192.168.1.99:8000/'
-// export const api = 'https://abdelrahmanecommerce.pythonanywhere.com/'
+export const api = 'https://abdelrahmanecommerce.pythonanywhere.com/'
 
 
 // export const clientUrl = 'http://localhost:5173/'
