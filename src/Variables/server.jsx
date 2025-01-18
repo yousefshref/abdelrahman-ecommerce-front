f9d0e460865ef347b1fcd9b480107ffd54fc300f
yousefshref <yb2005at@gmail.com>
2025-01-18 02:02:39 +0200

    a

--- src/Variables/server.jsx
+++ src/Variables/server.jsx
@@ -1,4 +1,5 @@
-export const api = 'http://93.127.213.60/'
+export const api = 'https://93.127.213.60/'
+// export const api = 'http://93.127.213.60/'
 // export const api = 'http://93.127.213.60:8000/'
 // export const api = 'http://127.0.0.1:8000/'
 // export const api = 'http://192.168.1.99:8000/'
